382dcdf0413e7eef5caa1bcc251833e4fcfe3ff0
miguelsndc <mglsndc@gmail.com>
2024-07-28 21:41:15 -0300

    refactor ray sphere intersection

--- src/core/ray.rs
+++ src/core/ray.rs
@@ -6,24 +6,6 @@ pub struct Ray {
     direction: Vec3,
 }
 
-pub fn intersect(s: Sphere, r: Ray) -> Option<(f64, f64)> {
-    let a = r.direction() ^ r.direction();
-    let oc = s.center() - r.origin();
-    let b = -2.0 * (r.direction ^ (oc));
-    let c = (oc ^ oc) - s.radius() * s.radius();
-
-    let discriminant = b * b - 4.0 * a * c;
-
-    if discriminant < 0.0 {
-        return None;
-    }
-
-    let t1 = (-b - f64::sqrt(discriminant)) / 2.0 * a;
-    let t2 = (-b + f64::sqrt(discriminant)) / 2.0 * a;
-
-    return Some((t1, t2));
-}
-
 impl Ray {
     pub fn new(origin: Point, direction: Vec3) -> Self {
         Ray { origin, direction }
@@ -55,6 +37,24 @@ impl Transform for Ray {
 mod tests {
     use super::*;
 
+    fn dummy_intersect(s: Sphere, r: Ray) -> Option<(f64, f64)> {
+        let a = r.direction() ^ r.direction();
+        let oc = s.center() - r.origin();
+        let b = -2.0 * (r.direction ^ (oc));
+        let c = (oc ^ oc) - s.radius() * s.radius();
+
+        let discriminant = b * b - 4.0 * a * c;
+
+        if discriminant < 0.0 {
+            return None;
+        }
+
+        let t1 = (-b - f64::sqrt(discriminant)) / 2.0 * a;
+        let t2 = (-b + f64::sqrt(discriminant)) / 2.0 * a;
+
+        return Some((t1, t2));
+    }
+
     #[test]
     fn ray_initialization() {
         let origin = Point::new(1.0, 2.0, 3.0);
@@ -79,7 +79,7 @@ mod tests {
             let r = Ray::new(Point::new(0.0, 0.0, -5.0), Vec3::new(0.0, 0.0, 1.0));
             let s = Sphere::default();
 
-            let xs = intersect(s, r).unwrap();
+            let xs = dummy_intersect(s, r).unwrap();
 
             assert_eq!(xs.0, 4.0);
             assert_eq!(xs.1, 6.0);
@@ -88,7 +88,7 @@ mod tests {
             let r = Ray::new(Point::new(0.0, 1.0, -5.0), Vec3::new(0.0, 0.0, 1.0));
             let s = Sphere::default();
 
-            let xs = intersect(s, r).unwrap();
+            let xs = dummy_intersect(s, r).unwrap();
 
             assert_eq!(xs.0, 5.0);
             assert_eq!(xs.1, 5.0);
@@ -97,7 +97,7 @@ mod tests {
             let r = Ray::new(Point::new(0.0, 2.0, -5.0), Vec3::new(0.0, 0.0, 1.0));
             let s = Sphere::default();
 
-            let xs = intersect(s, r).unzip();
+            let xs = dummy_intersect(s, r).unzip();
 
             assert_eq!(xs.0, None);
             assert_eq!(xs.1, None);
@@ -106,7 +106,7 @@ mod tests {
             let r = Ray::new(Point::new(0.0, 0.0, 0.0), Vec3::new(0.0, 0.0, 1.0));
             let s = Sphere::default();
 
-            let xs = intersect(s, r).unwrap();
+            let xs = dummy_intersect(s, r).unwrap();
 
             assert_eq!(xs.0, -1.0);
             assert_eq!(xs.1, 1.0);
@@ -115,7 +115,7 @@ mod tests {
             let r = Ray::new(Point::new(0.0, 0.0, 5.0), Vec3::new(0.0, 0.0, 1.0));
             let s = Sphere::default();
 
-            let xs = intersect(s, r).unwrap();
+            let xs = dummy_intersect(s, r).unwrap();
 
             assert_eq!(xs.0, -6.0);
             assert_eq!(xs.1, -4.0);
